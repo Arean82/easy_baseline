b9e3cfbabe7300f3c5f48433b567b446926a3b08
arean82 <arean82@gmail.com>
2025-07-27 20:39:16 +0530

    updated

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :easy_baseline do
   url 'https://www.easysoftware.com'
   author_url 'https://www.easysoftware.com'
   description 'Allow to create a snapshot of a project in time.'
-  version '2.0'
+  version '3.0'
 end
 
 require_relative 'after_init'
